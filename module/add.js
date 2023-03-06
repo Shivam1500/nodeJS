d1fe0975a2769bc96d38ca26912f89b72de1168a
abhishek2116 <abhishek.pathak@algoscale.com>
2023-03-06 19:05:31 +0530

    first commit

---
+++ module/add.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+    return a + b;
+};
+
+// const addition = add(3, 9)
+
+module.exports = add;
---
+++ module/index.js
@@ -0,0 +1,6 @@
+const add = require('./add');
+let Name = "Abhishek";
+console.log(Name);
+// require('./add.js')
+const sum = add(5, 89);
+console.log(sum);
